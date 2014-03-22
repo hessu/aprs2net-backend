b4ef3f658cb53002af6a18405888c3da81b99848
Heikki Hannikainen <hessu@hes.iki.fi>
2014-03-22 10:04:33 +0200

    Decode some server information from full status

--- poller/aprs2net-dns.py
+++ poller/aprs2net-dns.py
@@ -6,6 +6,7 @@ import logging.config
 import ConfigParser
 import sys
 import traceback
+import types
 
 import requests
 import json
@@ -15,6 +16,9 @@ CONFIG_SECTION = 'dns'
 DEFAULT_CONF = {
     # Server polling interval
     'poll_interval': '60',
+    
+    # Max test result age
+    'max_test_result_age': '300',
     
     # Portal URL for downloading configs
     'portal_base_url': 'https://home.tomh.us:8001'
@@ -46,6 +50,7 @@ class DNSDriver:
         self.poll_interval = self.config.getint(CONFIG_SECTION, 'poll_interval')
         self.domains = self.config.get(CONFIG_SECTION, 'domains').split(',')
         self.pollers = self.config.get(CONFIG_SECTION, 'pollers').split(' ')
+        self.max_test_result_age = self.config.getint(CONFIG_SECTION, 'max_test_result_age')
         
         self.rhead = {'User-agent': 'aprs2net-dns/2.0'}
         self.http_timeout = 10.0
@@ -54,6 +59,9 @@ class DNSDriver:
         """
         Fetch full status from each of the masters
         """
+        
+        status_set = {}
+        
         for url in self.pollers:
             self.log.info("Fetching status: %s", url)
             
@@ -64,6 +72,7 @@ class DNSDriver:
                 d = r.content
             except Exception as e:
                 self.log.error("%s: HTTP full JSON status fetch: Connection error: %s", url, str(e))
+                continue
             
             if r.status_code != 200:
                 self.log.error("%s: HTTP full JSON status fetch: Status code %d", url, r.status_code)
@@ -72,12 +81,68 @@ class DNSDriver:
             t_end = time.time()
             t_dur = t_end - t_start
             
-            self.log.debug("%s: HTTP GET /api/full returned: %r", url, r.status_code)
+            self.log.debug("%s: HTTP GET /api/full returned: %r (%.3f s)", url, r.status_code, t_dur)
             
             try:
                 j = json.loads(d)
             except Exception as e:
                 self.log.error("%s: JSON parsing failed: %r", url, e)
+                continue
+            
+            self.check_returned_status(url, j, status_set)
+    
+    def check_returned_status(self, siteid, j, status_set):
+        """
+        Check if the returned full status set is any good
+        """
+        
+        if j.get('result') != 'ok':
+            self.log.error("%s: Full status JSON does not have result: ok", siteid)
+            return
+        
+        servers = j.get('servers')
+        if not servers:
+            self.log.error("%s: Full status JSON does not contain servers", siteid)
+            return
+        
+        if not type(servers) is list:
+            self.log.error("%s: Full status JSON: servers is not a list", siteid)
+            return
+        
+        for s in servers:
+            self.add_returned_server(siteid, s, status_set)
+    
+    def add_returned_server(self, siteid, s, status_set):
+        """
+        Add a single returned server to current status set
+        """
+        
+        cfg = s.get('config')
+        stat = s.get('status')
+        
+        if cfg == None or stat == None:
+            self.log.error("%s: Server in set, with config or status missing", siteid)
+            return
+        
+        id = cfg.get('id')
+        last_test = stat.get('last_test')
+        
+        if id == None or last_test == None:
+            self.log.error("%s: Server in set, with id or last_test missing", siteid)
+            return
+        
+        test_age = time.time() - last_test
+        if test_age > self.max_test_result_age:
+            self.log.error("%s: [%s] test age %.0f > %.0f", siteid, id, test_age, self.max_test_result_age)
+            return
+        
+        if id in status_set:
+            set = status_set[id]
+        else:
+            set = status_set[id] = {}
+        
+        
+        
     
     def poll(self):
         """
